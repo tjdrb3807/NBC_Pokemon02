63a245f904fc4bf1f17192d5714c976e301e873f
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-30 10:23:21 +0900

    🔧 코드베이스 설정

---
+++ NBC_Pokemon02/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  NBC_Pokemon02
+//
+//  Created by 전성규 on 12/30/24.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
