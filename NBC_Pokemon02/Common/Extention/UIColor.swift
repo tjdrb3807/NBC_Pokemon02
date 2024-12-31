e0aaeafd04ce7d90213019fe793ed224e24d8f51
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-31 13:22:11 +0900

    ✨ UIColor Extension 구현

---
+++ NBC_Pokemon02/Common/Extention/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  NBC_Pokemon02
+//
+//  Created by 전성규 on 12/30/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let mainRed = UIColor(red: 190/255, green: 30/255, blue: 40/255, alpha: 1.0)
+    static let darkRed = UIColor(red: 120/255, green: 30/255, blue: 30/255, alpha: 1.0)
+    static let cellBackground = UIColor(red: 245/255, green: 245/255, blue: 235/255, alpha: 1.0)
+}
