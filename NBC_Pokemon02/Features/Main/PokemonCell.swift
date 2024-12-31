118cd2fb9e4a9258292a87a587312158fd668c2d
Jeon_Seong_gyu <junsung3807@naver.com>
2024-12-31 13:23:19 +0900

    ✨ PokemonCell 구현

---
+++ NBC_Pokemon02/Features/Main/PokemonCell.swift
@@ -0,0 +1,44 @@
+//
+//  PokemonCell.swift
+//  NBC_Pokemon02
+//
+//  Created by 전성규 on 12/30/24.
+//
+
+import UIKit
+import Kingfisher
+
+final class PokemonCell: UICollectionViewCell {
+    static let identifier = "PokemonCell"
+    
+    private let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        backgroundColor = UIColor.cellBackground
+        layer.cornerRadius = 12.0
+        clipsToBounds = true
+        
+        contentView.addSubview(thumbnailImageView)
+        
+        thumbnailImageView.frame = contentView.bounds
+    }
+    
+    func configure(with url: URL) {
+        thumbnailImageView.kf.setImage(with: url)
+    }
+}
